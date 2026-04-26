1168750af2d3bbe43fc098094c9a140d99f452c1
rubelsec <rubelmrm1492@gmail.com>
2026-04-27 00:14:12 +0600

    loaded data

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Blogs from './components/Blogs/Blogs'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+ 
+
+  return (
+    <>
+   <Header></Header>
+   <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
