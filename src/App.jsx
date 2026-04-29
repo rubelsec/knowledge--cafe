f63964f37bedf0625f4c4faa6a50a23988422d4f
rubelsec <rubelmrm1492@gmail.com>
2026-04-29 15:16:36 +0600

    addded bookmarked

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import Blogs from './components/Blogs/Blogs'
 import Bookmarks from './components/Bookmarks/Bookmarks'
 import './App.css'
 import Header from './components/Header/Header'
+import { useState } from 'react'
 
-function App() {
+function App()
+
+{
+  const [bookmarks, setBookmarks] = useState([]);
+  const handleAddToBookmark = blog => {
+   const newBookmarks = [...bookmarks,blog];
+   setBookmarks(newBookmarks);
+  }
  
 
   return (
     <>
    <Header></Header>
-   <div className='md:flex'>
-    <Blogs></Blogs>
-    <Bookmarks></Bookmarks>
+   <div className='md:flex max-w-7xl mx-auto'>
+    <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
+    <Bookmarks bookmarks={bookmarks}></Bookmarks>
 
    </div>
     </>
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
+import PropTypes from "prop-types";
 
-
-const Blogs = () => {
+const Blogs = ({handleAddToBookmark}) => {
     const [blogs,setBlogs]=useState([]);
     useEffect(()=>{
-        fetch(' blogs.json')
+        fetch('blogs.json')
         .then (res=> res.json())
         .then(data=> setBlogs(data))
 
@@ -12,8 +13,18 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
         <h1 className="text-4xl"> Blogs:{blogs.length}</h1>
+       {
+        blogs.map(blog => <Blog
+             key={blog.id} 
+             blog={blog}
+             handleAddToBookmark={handleAddToBookmark}
+             ></Blog>)
+       }
         </div>
     );
 };
+Blogs.propTypes={
+    handleAddToBookmark:PropTypes.func
+}
 
 export default Blogs;
